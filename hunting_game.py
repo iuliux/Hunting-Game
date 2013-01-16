58bd64a7ad0149b7c125942252fc4cf0017611c6
Iulius Curt <iulius.curt@gmail.com>
2013-01-17 01:31:05 +0200

    Added prey's random movement

--- hunting_game.py
+++ hunting_game.py
@@ -1,4 +1,6 @@
 import cherrypy
+from cherrypy.process.plugins import Monitor
+
 import os
 import random
 import simplejson
@@ -23,6 +25,26 @@ class Cell:
         while (self.x, self.y) in already_filled:
             self.x = int(random.uniform(0, table_size))
             self.y = int(random.uniform(0, table_size))
+
+    def move(self, direction):
+        ''''Direction direction is represented as int, as follows:
+            0 -> Up
+            1 -> Right
+            2 -> Down
+            3 -> Left
+        '''
+        # Up
+        if direction == 0:
+            self.x -= 1
+        # Right
+        elif direction == 1:
+            self.y += 1
+        # Down
+        elif direction == 2:
+            self.x += 1
+        # Left
+        elif direction == 3:
+            self.y -= 1
 
     def __repr__(self):
         return '<td></td>'
@@ -68,24 +90,74 @@ class World(Thread):
         table = [[Cell() for k in xrange(World.N)] for k in xrange(World.N)]
 
         for i in self.hunters:
+            # print '>>>>>>>>>>', i.x, '-', i.y, '<<<<<<<<<<<<<'
             table[i.x][i.y] = i
         for i in self.prey:
+            # print '>>>>>>>>>>', i.x, '-', i.y, '<<<<<<<<<<<<<'
             table[i.x][i.y] = i
 
         return ''.join(['<tr>'+str(
                     ''.join([str(cell) for cell in row])
                 )+'</tr>\n' for row in table])
 
-    def run(self):
-        while True:
-            for i in self.prey:
-                direction = int(random.uniform(0, 4))
-                # i.move(direction)
+    def adjacent_cell(self, x, y, direction):
+        # Up
+        if direction == 0:
+            new_x = x - 1
+            if new_x < 0:
+                return None
+            else:
+                return (new_x, y)
+        # Right
+        elif direction == 1:
+            new_y = y + 1
+            if new_y >= World.N:
+                return None
+            else:
+                return (x, new_y)
+        # Down
+        elif direction == 2:
+            new_x = x + 1
+            if new_x >= World.N:
+                return None
+            else:
+                return (new_x, y)
+        # Left
+        elif direction == 3:
+            new_y = y - 1
+            if new_y < 0:
+                return None
+            else:
+                return (x, new_y)
 
-            time.sleep(1)
+    def empty_cell(self, pos):
+        for i in self.hunters:
+            if (i.x, i.y) == pos:
+                return False
+        for i in self.prey:
+            if (i.x, i.y) == pos:
+                return False
+        return True
 
 
 world = World()
+
+
+def iterate():
+    print "ENTERS"
+    for i in world.prey:
+        # print '   ', i
+        direction = int(random.uniform(0, 4))
+        # print '    DIR:', direction
+        new_pos = world.adjacent_cell(i.x, i.y, direction)
+        # print '    newPOS:', new_pos
+        while not new_pos or not world.empty_cell(new_pos):
+            direction = int(random.uniform(0, 4))
+            new_pos = world.adjacent_cell(i.x, i.y, direction)
+        i.move(direction)
+
+    # print "HULALA"
+        # time.sleep(1)
 
 
 class AjaxApp(object):
@@ -100,5 +172,8 @@ class AjaxApp(object):
         return simplejson.dumps(dict(repr=table))
 
 
+Monitor(cherrypy.engine, iterate, frequency=1).subscribe()
+
+cherrypy.config.update({'server.socket_port': 4040})
 cherrypy.tree.mount(AjaxApp(), '/', config=config)
 cherrypy.engine.start()
